66f0249685121158d2794f3d1cb1eb1ca8504de2
Manteiga <rafael.manteiga@hotmail.com>
2022-11-19 04:15:51 -0300

    Add files via upload

---
+++ covid60dias.R
@@ -0,0 +1,20 @@
+install.packages('ggplot2')
+install.packages('zoo')
+
+library(ggplot2)
+library(zoo)
+
+# Criando tabela com as médias móveis e armazenando em avg
+avg <- rollmean(dados,as.integer(sqrt(60)))
+
+# Convertendo a tabela para o tipo de dados correto
+avg <- data.frame(avg)
+
+# Criando gráfico com os pontos da média móvel (V1 é a coluna dentro da tabela)
+grafico1 = ggplot(data = avg,aes(x=row(avg),y=V1))
+
+# Criando linha de tendência a partir dos dados
+tendenciamediamovel = grafico1+geom_point(size=2.5)+geom_smooth(fill=NA)
+
+# Plotando o gráfico
+plot(tendenciamediamovel)
